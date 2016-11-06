a6f91304a8dc3be696608d9d012e10d491ad1745
RazvanJivulescu <rjivulescu@yahoo.com>
2016-11-06 19:50:36 +0200

    Repository

---
+++ PSSC/Models/Repository/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Models.Student;
+
+namespace Models.Repository
+{
+    // Repository for GradeReport aggregat root
+    class GradeReportRepository : IRepository<Student.GradeReport>
+    {
+        public IEnumerable<GradeReport> List
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(GradeReport entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(GradeReport entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GradeReport FindById(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GradeReport entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
